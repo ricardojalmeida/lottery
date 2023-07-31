644417790a4a68f5326d6e060c692eb0ecb8827d
Ricardo Jorge Almeida <113598600+ricardojalmeida@users.noreply.github.com>
2023-07-31 14:17:47 +0300

    Update Lottary.sol

--- src/contracts/Lottary.sol
+++ src/contracts/Lottary.sol
@@ -1,21 +1,21 @@
-pragma solidity ^0.8.11;
-contract Lottery {
+pragma solidity ^0.8.0;
+
+contract PaymentContract {
     address payable public owner; // the address of the owner of the contract
-    address payable[] public players; // an array of addresses of the players in the lottery
-    uint public fee = 0.1 ether; // the fee required to enter the lottery
+    address payable[] public recipient;
+    uint public fee = 0.1 ether; 
 
-    constructor() {
+    constructor(){
         owner = payable (msg.sender);
-        lotteryId = 1;
     }
 
-    function getPlayers() public view returns (address payable[] memory) {
-        return players; // get the array of addresses of the players in the lottery
-    }
-    function enter() public payable {
-        require(msg.value > fee); // require that the value sent with the transaction is greater than the fee
-        // add the address of the player entering the lottery to the array of players
-        players.push(payable(msg.sender));
+    function sendPayment() public payable {
+        require (msg.value > fee);
+        owner.transfer(msg.value);
+        recipient.push(payable(msg.sender));
     }
 
+        function getUsers() public view returns (address payable[] memory) {
+        return recipient; // get the array of addresses of the players in the lottery
+    }
 }
